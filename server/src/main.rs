4ff1b8fe6517a2e40b86ec4ce630e7158568f0e2
Joel Sikström <joel.attano@gmail.com>
2021-04-06 18:19:58 +0200

    Initial web server setup (#4)
    * Initial implementation of the web server with support for Websockets.
    
    The web server now only exposes one endpoint, "/ws", where a client can
    open a websocket session by making a request to "ws://<ip>:8080/ws".
    
    The websocket implementation supports 1) heartbeat 2) common/shared
    state between clients and 3) possibility to send internal messages from
    client actors to the lobby (shared state).
    
    * Updated .gitignore to ignore rust/cargo project files.
    
    * Improved documentation with more detail in descriptions.

---
+++ server/src/main.rs
@@ -0,0 +1,21 @@
+mod endpoints;
+mod lobby;
+mod messages;
+mod ws;
+
+use actix::Actor;
+use actix_web::{App, HttpServer};
+use endpoints::ws_endpoint as ws_endpoint_route;
+use lobby::Lobby;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Create the common/shared state.
+    let lobby = Lobby::default().start();
+
+    HttpServer::new(move || App::new().service(ws_endpoint_route).data(lobby.clone()))
+        // The "0.0.0.0" means that the server accepts requests from any host (127.0.0.1, 192.168.x.x, etc..)
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
+}
