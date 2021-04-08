76c49be6b5e5cd01d12967952cac4b18b437d447
Karl Thomsson <karl@DESKTOP-KR3FRMM.localdomain>
2021-04-08 19:51:39 +0200

    refactoring, changed google maps library

--- client/src/map.js
+++ client/src/map.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { useState } from "react";
-import GoogleMapReact from "google-map-react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 import Fab from "@material-ui/core/Fab";
-import Paper from "@material-ui/core/Paper";
 import Brightness3Icon from "@material-ui/icons/Brightness3";
 import MyLocationIcon from "@material-ui/icons/MyLocation";
-import PersonPinCircleIcon from "@material-ui/icons/PersonPinCircle";
 import "./App.css";
 
 function Map(props) {
-
   const defaultLat = 59.8585;
   const defaultLng = 17.6389;
   const defaultCenter = {
@@ -17,34 +19,32 @@ function Map(props) {
     lng: defaultLng,
   };
   const styles = require("./mapstyle.json");
-  const Marker = () => <div className="marker">&#128652;</div>;
-  const CenterMarker = () => (
-    <div className="centerMarker">
-      <PersonPinCircleIcon color="secondary" />
-    </div>
-  );
-
-  const displayInfo = (obj) => {
-    setBusInfo(<Paper 
-      id="busInfo"
-      lat={obj.position.latitude} 
-      lng={obj.position.longitude}>
-        bus {obj.id}
-      </Paper>);
-  };
-
-  const clearInfo = () => {
-    setBusInfo('');
-  };
-
-  const [busInfo, setBusInfo] = useState();
   const [currentTheme, setCurrentTheme] = useState(styles.day);
   const [realtimeData, setRealtimeData] = useState(props.realtimeData);
   const [currentCenter, setCurrentCenter] = useState(defaultCenter);
-  
-  
+  const [selectedMarker, setSelectedMarker] = useState(null);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyD2nxWN__H5vzHRFdYejT9v8_qLv9R8vUk",
+  });
+
+  const mapContainerStyle = {
+    height: "100vh",
+    width: "100vw",
+  };
+
+  const options = {
+    styles: currentTheme,
+    disableDefaultUI: true,
+  };
+
   const updateLocation = () => {
-    if(navigator.geolocation) {
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(setCoordinates);
     } else {
       alert("Browser error");
@@ -54,68 +54,89 @@ function Map(props) {
   const setCoordinates = (position) => {
     setCurrentCenter({
       lat: position.coords.latitude,
-      lng: position.coords.longitude
+      lng: position.coords.longitude,
     });
   };
 
   const changeTheme = () => {
     if (currentTheme === styles.day) {
-        setCurrentTheme(styles.night)
+      setCurrentTheme(styles.night);
     } else {
-        setCurrentTheme(styles.day)
+      setCurrentTheme(styles.day);
     }
-  }
+  };
 
   var arr = [
     {
       id: 1,
       position: {
-        latitude:  59.8595,
-        longitude: 17.6389
-      }
+        latitude: 59.8595,
+        longitude: 17.6389,
+      },
     },
     {
       id: 2,
       position: {
         latitude: 59.8575,
-        longitude: 17.6399
-      }
-    }
+        longitude: 17.6399,
+      },
+    },
   ];
 
-  return (
-    <div style={{ height: "100vh", width: "100%" }}>
-      <GoogleMapReact
-        onClick={clearInfo}
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={defaultCenter}
-        defaultZoom={15}
-        center={currentCenter}
-        options={{
-          styles: currentTheme,
-          disableDefaultUI: true,
-          fullscreenControl: false,
-        }}
-      >
-        {arr.map((obj, i) => (
-          <div 
-          className="marker"
-          key={obj.id}
-          lat={obj.position.latitude}
-          lng={obj.position.longitude}
-          onClick= {() => {displayInfo(obj)}}
-          >
-            &#128652;
-          </div>
-          
-        ))}
-        {busInfo}
-        <CenterMarker 
-        lat={currentCenter.lat}
-        lng={currentCenter.lng}
-         />
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading...";
 
-      </GoogleMapReact>
+  return (
+    <div>
+      <GoogleMap
+        zoom={15}
+        center={currentCenter}
+        mapContainerStyle={mapContainerStyle}
+        options={options}
+        onLoad={onMapLoad}
+      >
+        {arr.map((obj) => (
+          <Marker
+            key={obj.id}
+            position={{
+              lat: obj.position.latitude,
+              lng: obj.position.longitude,
+            }}
+            onClick={() => {
+              setSelectedMarker(obj);
+            }}
+          >
+          </Marker>
+        ))}
+        {selectedMarker && (
+          <InfoWindow
+            position={{
+              lat: selectedMarker.position.latitude,
+              lng: selectedMarker.position.longitude,
+            }}
+            onCloseClick={() => {
+              setSelectedMarker(null);
+            }}
+          >
+            <div>
+              <p>{`Bus ${selectedMarker.id} \n Passengers 15`}</p>
+            </div>
+          </InfoWindow>
+        )}
+
+        <Marker
+          position={{
+            lat: currentCenter.lat,
+            lng: currentCenter.lng,
+          }}
+          icon={{
+            url: "/personpin.svg",
+            origin: new window.google.maps.Point(0, 0),
+            anchor: new window.google.maps.Point(15, 15),
+            scaledSize: new window.google.maps.Size(30, 30),
+          }}
+        />
+      </GoogleMap>
       <Fab
         id="locationButton"
         color="primary"
@@ -124,11 +145,7 @@ function Map(props) {
       >
         <MyLocationIcon />
       </Fab>
-      <Fab
-        color="primary"
-        id="themeButton"
-        onClick = {changeTheme}
-      >
+      <Fab color="primary" id="themeButton" onClick={changeTheme}>
         <Brightness3Icon />
       </Fab>
     </div>
