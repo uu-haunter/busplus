d413889d17e206346c0374541bf4f96bc0bb0e6a
cawe <carl@willman.nu>
2021-04-12 17:42:34 +0200

    Documentation fixes

--- server/src/database.rs
+++ server/src/database.rs
@@ -1,13 +1,15 @@
+//! Module for handling operations and connection to a external MongoDB database
 use mongodb::{error::Error, options::ClientOptions, Client};
 
+///Constant of the database name that should be used
 const DATABASE_NAME: &str = "haunterDB";
 
-//Our abstraction for the db, we can use method syntax for operation ex: conn.updateGeoPosition(id, value)
+///Our abstraction for the db, we can use method syntax for operation ex: conn.updateGeoPosition(id, value)
 pub struct Connection {
     client: Client,
 }
 
-//Inititalise a connection with uri_str
+///Inititalise a connection with uri_str
 pub async fn init_connection(uri_str: &str) -> Result<Connection, mongodb::error::Error> {
     let client_options = ClientOptions::parse(uri_str).await?;
     let result_client = Client::with_options(client_options)?;
