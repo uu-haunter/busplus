8b783241ff6aea3dae42f897abe47fae55cfbb0b
Karl Thomsson <61412812+thomssonkarl@users.noreply.github.com>
2021-04-23 14:56:12 +0200

    Added some zoom/centering functionality to personpin, support for pulsating personpin icon and deleted some uneccesary icon-files from public directory (#50)

--- client/src/map.js
+++ client/src/map.js
@@ -5,9 +5,9 @@ import {
   useLoadScript,
   Marker,
   InfoWindow,
-  Polyline
+  Polyline,
 } from "@react-google-maps/api";
-import { computeDistanceBetween, interpolate } from 'spherical-geometry-js';
+import { computeDistanceBetween, interpolate } from "spherical-geometry-js";
 import Fab from "@material-ui/core/Fab";
 import Brightness3Icon from "@material-ui/icons/Brightness3";
 import MyLocationIcon from "@material-ui/icons/MyLocation";
@@ -18,7 +18,6 @@ import "./App.css";
  */
 
 function Map(props) {
-
   const defaultLat = 59.8585;
   const defaultLng = 17.6389;
   const defaultCenter = {
@@ -29,33 +28,36 @@ function Map(props) {
   // State-variables
   const styles = require("./mapstyle.json");
   const [currentTheme, setCurrentTheme] = useState(styles.day);
-  const [vehicleData, setVehicleData] = useState({timestamp: null, vehicles: {}});
+  const [vehicleData, setVehicleData] = useState({
+    timestamp: null,
+    vehicles: {},
+  });
   const [currentCenter, setCurrentCenter] = useState(defaultCenter);
   const [selectedMarker, setSelectedMarker] = useState(null);
   const [currentRoute, setRoute] = useState(null);
 
   // Options that specify the route-drawing
   const polyLineOptions = {
-    strokeColor: '#FF0000',
+    strokeColor: "#FF0000",
     strokeOpacity: 0.8,
     strokeWeight: 2,
-    fillColor: '#FF0000',
+    fillColor: "#FF0000",
     fillOpacity: 0.35,
     clickable: false,
     draggable: false,
     editable: false,
     visible: true,
     radius: 30000,
-    zIndex: 1
+    zIndex: 1,
   };
 
   // Returns a route request message for a specific bus line
   const routeRequest = (lineNo) => {
     return {
-        "type": "get-route-info",
-        "payload": {
-          "line": lineNo
-        }
+      type: "get-route-info",
+      payload: {
+        line: lineNo,
+      },
     };
   };
 
@@ -69,33 +71,34 @@ function Map(props) {
       let serverUpdateInterval = 5000;
 
       // The time that has passed since the last realtime update was received.
-      let dt = (new Date().getTime() - vehicleData.timestamp);
+      let dt = new Date().getTime() - vehicleData.timestamp;
 
       // Dividing the time delta with the time interval of realtime updates
       // in order to get the fraction of the way that the vehicle should have
       // reached if it moves at a constant rate of speed.
       let fraction = dt / serverUpdateInterval;
 
-      if(fraction > 1) return;
+      if (fraction > 1) return;
 
-      setVehicleData(
-        {
-          timestamp: vehicleData.timestamp,
-          vehicles: Object.fromEntries(
-            Object.entries(vehicleData.vehicles).map(([vehicleId, vehicle]) => {
+      setVehicleData({
+        timestamp: vehicleData.timestamp,
+        vehicles: Object.fromEntries(
+          Object.entries(vehicleData.vehicles).map(([vehicleId, vehicle]) => {
+            // interpolate between the source and target positions using the calculated fraction
+            // to get the new position.
+            let newLatLng = interpolate(
+              vehicle.sourcePosition,
+              vehicle.targetPosition,
+              fraction
+            );
 
-              // interpolate between the source and target positions using the calculated fraction
-              // to get the new position.
-              let newLatLng = interpolate(vehicle.sourcePosition, vehicle.targetPosition, fraction);
+            vehicle.currentPosition.latitude = newLatLng.lat();
+            vehicle.currentPosition.longitude = newLatLng.lng();
 
-              vehicle.currentPosition.latitude = newLatLng.lat();
-              vehicle.currentPosition.longitude = newLatLng.lng();
-
-              return [vehicleId, vehicle];
-            })
-          )
-        }
-      );
+            return [vehicleId, vehicle];
+          })
+        ),
+      });
     }, ms);
 
     return () => {
@@ -103,29 +106,29 @@ function Map(props) {
     };
   }, [vehicleData, vehicleData.vehicles]);
 
-  
   useEffect(() => {
-    setVehicleData(
-      {
-        timestamp: new Date().getTime(),
-        vehicles: Object.fromEntries(
-          props.realtimeData.map(vehicle => {
-            let vehicleId = vehicle.descriptorId.toString();
-            let entry = vehicleData.vehicles[vehicleId];
+    setVehicleData({
+      timestamp: new Date().getTime(),
+      vehicles: Object.fromEntries(
+        props.realtimeData.map((vehicle) => {
+          let vehicleId = vehicle.descriptorId.toString();
+          let entry = vehicleData.vehicles[vehicleId];
 
-            return [
-              vehicleId,
-              {
-                sourcePosition: ( entry ? {...entry.targetPosition} : {...vehicle.position} ),
-                currentPosition: ( entry ? {...entry.targetPosition} : {...vehicle.position} ),
-                targetPosition: {...vehicle.position}
-              }
-            ];
-
-          })
-        )
-      }
-    );
+          return [
+            vehicleId,
+            {
+              sourcePosition: entry
+                ? { ...entry.targetPosition }
+                : { ...vehicle.position },
+              currentPosition: entry
+                ? { ...entry.targetPosition }
+                : { ...vehicle.position },
+              targetPosition: { ...vehicle.position },
+            },
+          ];
+        })
+      ),
+    });
   }, [props.realtimeData]);
 
   // Hook used to modify the route-data
@@ -168,12 +171,11 @@ function Map(props) {
 
     // return the distance along the earths surface
     return computeDistanceBetween(center, northEast);
-  }
+  };
 
-  
   const { isLoaded, loadError } = useLoadScript({
     // Reads the google-maps api_key from your locally created .env file
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
 
   // Container size for the GoogleMap component
@@ -204,7 +206,7 @@ function Map(props) {
       lng: position.coords.longitude,
     });
   };
-  
+
   // Changes between dark-theme and light-theme
   const changeTheme = () => {
     if (currentTheme === styles.day) {
@@ -224,37 +226,40 @@ function Map(props) {
         center={currentCenter}
         mapContainerStyle={mapContainerStyle}
         options={options}
-        onClick={()=>{
+        onClick={() => {
           setSelectedMarker(null);
           setRoute([]);
         }}
         onLoad={onMapLoad}
         onBoundsChanged={onBoundsChanged}
       >
-        {
-          Object.entries(vehicleData.vehicles).map(([vehicleId, vehicle]) => (
-            <Marker
-              key={vehicleId}
-              position={{
-                lat: vehicle.currentPosition.latitude,
-                lng: vehicle.currentPosition.longitude
-              }}
-              onClick={() => {
-                setSelectedMarker(vehicleId);
-                // TODO: Change argument for routeRequest when we have line data 
-                props.wsSend(JSON.stringify(routeRequest("30")));
-              }}
-              
-            >
-            </Marker>
-          ))
-
-        }
+        {Object.entries(vehicleData.vehicles).map(([vehicleId, vehicle]) => (
+          <Marker
+            key={vehicleId}
+            position={{
+              lat: vehicle.currentPosition.latitude,
+              lng: vehicle.currentPosition.longitude,
+            }}
+            onClick={() => {
+              setSelectedMarker(vehicleId);
+              // TODO: Change argument for routeRequest when we have line data
+              props.wsSend(JSON.stringify(routeRequest("30")));
+            }}
+            icon={{
+              url: "/bus.svg",
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+              scaledSize: new window.google.maps.Size(30, 30),
+            }}
+          ></Marker>
+        ))}
         {selectedMarker && (
           <InfoWindow
             position={{
-              lat: vehicleData.vehicles[selectedMarker].currentPosition.latitude,
-              lng: vehicleData.vehicles[selectedMarker].currentPosition.longitude,
+              lat:
+                vehicleData.vehicles[selectedMarker].currentPosition.latitude,
+              lng:
+                vehicleData.vehicles[selectedMarker].currentPosition.longitude,
             }}
             onCloseClick={() => {
               setSelectedMarker(null);
@@ -271,30 +276,40 @@ function Map(props) {
             lat: currentCenter.lat,
             lng: currentCenter.lng,
           }}
+          onClick={() => {
+            mapRef.current.setZoom(18);
+            updateLocation();
+          }}
           icon={{
             url: "/personpin.svg",
             origin: new window.google.maps.Point(0, 0),
             anchor: new window.google.maps.Point(15, 15),
+            //Change Size to (150,150) when using pulsating circle icon
             scaledSize: new window.google.maps.Size(30, 30),
           }}
         />
 
-        {currentRoute && (<Polyline
-        path={currentRoute.map(obj=>{
-          return ({
-            // TODO: Message should send coords in number format instead of string
-            lat: parseFloat(obj.lat),
-            lng: parseFloat(obj.lng)
-          })
-        })}
-        options={polyLineOptions}
-        />)}
+        {currentRoute && (
+          <Polyline
+            path={currentRoute.map((obj) => {
+              return {
+                // TODO: Message should send coords in number format instead of string
+                lat: parseFloat(obj.lat),
+                lng: parseFloat(obj.lng),
+              };
+            })}
+            options={polyLineOptions}
+          />
+        )}
       </GoogleMap>
       <Fab
         id="locationButton"
         color="primary"
         aria-label="locationButton"
-        onClick={updateLocation}
+        onClick={() => {
+          mapRef.current.setZoom(18);
+          updateLocation();
+        }}
       >
         <MyLocationIcon />
       </Fab>
